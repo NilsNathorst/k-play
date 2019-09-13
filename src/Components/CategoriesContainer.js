0f22cd741351046968f849450e405b36832a076f
Neyrin <isa.areschoug@gmail.com>
2019-09-13 10:54:04 +0200

    Removed routing in component

--- src/Components/CategoriesContainer.js
+++ src/Components/CategoriesContainer.js
@@ -16,30 +16,17 @@ const categories = [
   { label: "film", name: "Film", value: filterContentByTag(videos, "film") }
 ];
 
-
-const StyledCategoriesContainer = styled.div`
-  width: 100%;
-  height: 20vh;
-  padding: 20px;
-  background-color: #000;
-  color: #fff;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
 class CategoriesContainer extends Component {
   render() {
     return (
-      <StyledCategoriesContainer>
-  
-          {categories.map(category => (
-            <p>
-            <Link to={`/content/${category.label}`}>{category.name}</Link>
-            </p>
-          ))}
+      <>
+        {categories.map(category => (
+          <p>
+          <Link to={`/content/${category.label}`}>{category.name}</Link>
+          </p>
+        ))}
+      </>
         
-      </StyledCategoriesContainer>
     );
   }
 }
