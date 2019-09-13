dd18315b666ae6916150b56edadd1db35b49b78e
Neyrin <isa.areschoug@gmail.com>
2019-09-13 13:49:26 +0200

    Removed redundant imports, added map for content

--- src/components/CategoriesContainer.js
+++ src/components/CategoriesContainer.js
@@ -1,33 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import customStyles from "../styles/customStyle";
 import tracks from "../data/tracks";
 import videos from "../data/youtube";
-import Video from "../views/Video";
-const allContent = [tracks, videos].flat();
-console.log(allContent);
 const filterContentByTag = (contentArray, tag) => {
   let newArr = contentArray.filter(content => content.tags.includes(tag));
   return newArr;
 };
 const categories = [
-  { label: "dans", value: filterContentByTag(videos, "dans") },
-  { label: "teater", value: filterContentByTag(videos, "teater") },
-  { label: "film", value: filterContentByTag(videos, "film") }
+  { label: "dans", name: "Dans", value: filterContentByTag(videos, "dans") },
+  { label: "teater", name: "Teater", value: filterContentByTag(videos, "teater") },
+  { label: "film", name: "Film", value: filterContentByTag(videos, "film") }
 ];
+
 class CategoriesContainer extends Component {
   render() {
     return (
       <>
-        <Router>
-          {categories.map(category => (
-            <p>
-            <Link to={`/category/${category.label}`}>{category.label}</Link>
-            </p>
-          ))}
-          <Route exact path="/category/:categoryId" component={Video} />
-        </Router>
+        {categories.map(category => (
+          <p>
+          <Link to={`/selectedcontent/${category.label}`}>{category.name}</Link>
+          </p>
+        ))}
       </>
+        
     );
   }
 }
