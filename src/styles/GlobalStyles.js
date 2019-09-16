5fcbad899dd157384a12560f912666c8ee56befb
Nils <nilsnathorstwindahl@gmail.com>
2019-09-16 10:41:49 +0200

    updated theme

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,13 +1,27 @@
 import { createGlobalStyle } from "styled-components";
+import theme from "./Theme";
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,900&display=swap');
   *,*::before,*::after {
     box-sizing: border-box;
     margin: 0;
     padding: 0;
     position: relative;
     font-family: Roboto;
+  }
+  body {
+    background-color: ${theme.colorDark};
+    padding: 1.3rem 1rem 2.6rem 1rem;
+  }
+  #root{
+    overflow-x: hidden;
+  }
+  h1 {
+    color: ${theme.colorLight};
+    font-size: 52px;
+    letter-spacing: -1.2px;
+    font-weight: 900;
   }
 `;
 
