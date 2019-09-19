769bed2d779373ff2d8ef1d2f78deb37a168b610
Neyrin <isa.areschoug@gmail.com>
2019-09-19 08:42:41 +0200

    Added logic for shown content

--- src/views/SelectedContent.js
+++ src/views/SelectedContent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Search from "../components/Search";
 import tracks from "../data/tracks";
 import videos from "../data/youtube";
+import VideosContainer from '../components/VideosContainer';
 const filterContentByTag = (contentArray, tag) => {
   let newArr = contentArray.filter(content => content.tags.includes(tag));
   return newArr;
@@ -10,20 +11,37 @@ class SelectedContent extends Component {
   render() {
     const { category } = this.props.match.params
     const SelectedContent = filterContentByTag(videos, category);
-    console.log(SelectedContent);
-    return (
-      <>
-      < Search />
-
-      {SelectedContent.map(category => (
+    const allVideos = videos;
+    //console.log(allVideos);
+    //console.log(SelectedContent);
+    if (!category){
+      return (
         <>
-          <p key={category.id}>{category.title}</p>
-          <img src={category.thumbnail}></img>
-        </>
-      ))}
-      </>  
+          <Search />
 
-    );
+          {allVideos.map(video => (
+            <>
+              <p key={video.id}>{video.title}</p>
+              <img src={video.thumbnail}></img>
+            </>
+          ))}
+        </>
+      )
+    } else {
+      return (
+        <>
+          < Search />
+
+          {SelectedContent.map(category => (
+            <>
+              <p key={category.id}>{category.title}</p>
+              <img src={category.thumbnail}></img>
+            </>
+          ))}
+        </>  
+
+      );
+    }
   }
 }
 
