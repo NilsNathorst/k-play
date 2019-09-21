a34c4e0dba0eaad4bbd3ecaf210c1154a8663c9f
Nils <nilsnathorstwindahl@gmail.com>
2019-09-21 14:00:56 +0200

    renamed

--- src/components/SingleVideo.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react";
-
-const SingleVideo = ({ match }) => {
-  console.log(match);
-  useEffect(() => {}, []);
-  return <div>hej</div>;
-};
-
-export default SingleVideo;
