5bf13875a33882d45f4e803d55337815b0476580
Nils <nilsnathorstwindahl@gmail.com>
2019-09-24 20:35:51 +0200

    updated categories and layout

--- src/components/CategoriesContainer.js
+++ src/components/CategoriesContainer.js
@@ -1,29 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
-
-const categories = [
-  { label: "dans", name: "Dans" },
-  {
-    label: "teater",
-    name: "Teater"
-  },
-  { label: "film", name: "Film" },
-  { label: "all", name: "Alla videor" }
-];
+import tags from "../data/tags";
+const categories = [...new Set(tags.flat())];
 
 const Container = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
+  .primary-cta {
+    padding: ${({ theme }) => theme.padding2};
+  }
+  ${props =>
+    props.secondary &&
+    `
+    margin-top: 20vh;
+      flex-direction: column;
+      justify-content: flex-end;
+      margin-bottom: 42vh;
+      .primary-cta {
+   text-align: right;
+   text-decoration: underline;
+   margin-top: 12px;
+  }
+  .primary-cta:after {
+display: none;
+  }
+    `}
 `;
 
 const CategoriesContainer = props => {
   return (
-    <Container>
-      {categories.map((category, i) => (
-        <Link key={i} to={`/selectedcontent/${category.label}`}>
-          <p>{category.name}</p>
+    <Container secondary={props.secondary}>
+      {categories.slice(0, 8).map((category, i) => (
+        <Link key={i} to={`/selectedcontent/${category}`}>
+          <p className="primary-cta">{category}</p>
         </Link>
       ))}
     </Container>
