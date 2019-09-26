d9e398a4d47e205c8942dbe3819b8c596e459de3
Nils <nilsnathorstwindahl@gmail.com>
2019-09-26 13:38:04 +0200

    fixed overflow hamburger

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,7 +3,10 @@ import theme from "./Theme";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap');
-
+.hidden {
+  height: 100vh;
+  overflow-y: hidden;
+}
   *,*::before,*::after {
     box-sizing: border-box;
     margin: 0;
